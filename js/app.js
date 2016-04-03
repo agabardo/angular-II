6fc835eeced3ed21ad7c8fc8acf4fc5df17e843d
Ademir C. Gabardo <ademir.gabbardo@gmail.com>
2016-04-03 19:17:13 +1000

    Warming up

--- js/app.js
+++ js/app.js
@@ -7,15 +7,22 @@
   var gems = [ {
     name : "Dodecadron",
     price: 2.95,
-    description: "A beaultiful gem.",
+    description: "A beaultiful gem. A beaultiful gem. A beaultiful gem.",
     canPurchase: true,
     soldOut: false
   },
   {
     name : "Pentagonal Gem",
     price: 5.95,
-    description: "Yet, another beaultiful gem.",
-    canPurchase: false,
-    soldOut: true
+    description: "Yet, another beaultiful gem. Yet, another beaultiful gem. Yet, another beaultiful gem.",
+    canPurchase: true,
+    soldOut: false
+  },
+  {
+    name : "Pentagonalx Gem",
+    price: 5.95,
+    description: "Yet, another beaultiful gem. Yet, another beaultiful gem. Yet, another beaultiful gem.",
+    canPurchase: true,
+    soldOut: false
   }];
 })();
