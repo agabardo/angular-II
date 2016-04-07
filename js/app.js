8614db8042fcc670d2a0678197e4e4a3fbb9bbab
Ademir C. Gabardo <ademir.gabbardo@gmail.com>
2016-04-07 19:05:06 +1000

    Updating

--- js/app.js
+++ js/app.js
@@ -21,40 +21,87 @@
   app.controller('StoreController', function(){
       this.products = gems;
   });
-  var gems = [ {
-    name : "Dodecadron",
-    price: 2.95,
-    description: "A beaultiful gem. A beaultiful gem. A beaultiful gem.",
-    canPurchase: true,
-    soldOut: false,
-    images:[
-      {full:"images/dodecadron-01-full.gif",thumb:"images/dodecadron-01-thumb.gif"},
-      {full:"images/dodecadron-02-full.gif",thumb:"images/dodecadron-02-thumb.gif"},
-      {full:"images/dodecadron-03-full.gif",thumb:"images/dodecadron-03-thumb.gif"}
-    ]
-  },
-  {
-    name : "Pentagonal Gem",
-    price: 5.95,
-    description: "Yet, another beaultiful gem. Yet, another beaultiful gem. Yet, another beaultiful gem.",
-    canPurchase: true,
-    soldOut: false,
-    images:[
-      {full:"images/pentagonal-01-full.gif",thumb:"images/pentagonal-01-thumb.gif"},
-      {full:"images/pentagonal-02-full.gif",thumb:"images/pentagonal-02-thumb.gif"},
-      {full:"images/pentagonal-03-full.gif",thumb:"images/pentagonal-03-thumb.gif"}
-    ]
-  },
-  {
-    name : "Pentagonalx Gem",
-    price: 5.95,
-    description: "Yet, another beaultiful gem. Yet, another beaultiful gem. Yet, another beaultiful gem.",
-    canPurchase: true,
-    soldOut: false,
-    images:[
-      {full:"images/pentagonalx-01-full.gif",thumb:"images/pentagonalx-01-thumb.gif"},
-      {full:"images/pentagonalx-02-full.gif",thumb:"images/pentagonalx-02-thumb.gif"},
-      {full:"images/pentagonalx-03-full.gif",thumb:"images/pentagonalx-03-thumb.gif"}
-    ]
-  }];
+
+  var gems = [
+      {
+        name: 'Azurite',
+        description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
+        shine: 8,
+        price: 110.50,
+        rarity: 7,
+        color: '#CCC',
+        faces: 14,
+        images: [
+          "images/gem-02.gif",
+          "images/gem-05.gif",
+          "images/gem-09.gif"
+        ],
+        reviews: [{
+          stars: 5,
+          body: "I love this gem!",
+          author: "joe@example.org",
+          createdOn: 1397490980837
+        }, {
+          stars: 1,
+          body: "This gem sucks.",
+          author: "tim@example.org",
+          createdOn: 1397490980837
+        }]
+      },
+      {
+        name: 'Bloodstone',
+        description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
+        shine: 9,
+        price: 22.90,
+        rarity: 6,
+        color: '#EEE',
+        faces: 12,
+        images: [
+          "images/gem-01.gif",
+          "images/gem-03.gif",
+          "images/gem-04.gif",
+        ],
+        reviews: [{
+          stars: 3,
+          body: "I think this gem was just OK, could honestly use more shine, IMO.",
+          author: "JimmyDean@example.org",
+          createdOn: 1397490980837
+        }, {
+          stars: 4,
+          body: "Any gem with 12 faces is for me!",
+          author: "gemsRock@example.org",
+          createdOn: 1397490980837
+        }]
+      },
+      {
+        name: 'Zircon',
+        description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
+        shine: 70,
+        price: 1100,
+        rarity: 2,
+        color: '#000',
+        faces: 6,
+        images: [
+          "images/gem-06.gif",
+          "images/gem-07.gif",
+          "images/gem-09.gif"
+        ],
+        reviews: [{
+          stars: 1,
+          body: "This gem is WAY too expensive for its rarity value.",
+          author: "turtleguyy@example.org",
+          createdOn: 1397490980837
+        }, {
+          stars: 1,
+          body: "BBW: High Shine != High Quality.",
+          author: "LouisW407@example.org",
+          createdOn: 1397490980837
+        }, {
+          stars: 1,
+          body: "Don't waste your rubles!",
+          author: "nat@example.org",
+          createdOn: 1397490980837
+        }]
+      }
+    ];
 })();
