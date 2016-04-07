849551a9cf9d785e7608426cbc622f20c9ffef00
Ademir C. Gabardo <ademir.gabbardo@gmail.com>
2016-04-07 17:50:17 +1000

    Updating

--- js/app.js
+++ js/app.js
@@ -1,28 +1,60 @@
 (function(){
   var app = angular.module('store',[]);
+
+  app.controller("PannelController", function(){
+    this.tab = 1;
+    this.selectTab = function(setTab){
+      this.tab = setTab;
+    }
+    this.isSelected = function(checkTab){
+      return this.tab === checkTab;
+    }
+  });
+
+  app.controller('GalleryController', function(){
+    this.current = 0;
+    this.setCurrent = function(newGallery){
+      this.current = newGallery || 0;
+    };
+  });
+
   app.controller('StoreController', function(){
       this.products = gems;
   });
-
   var gems = [ {
     name : "Dodecadron",
     price: 2.95,
     description: "A beaultiful gem. A beaultiful gem. A beaultiful gem.",
     canPurchase: true,
-    soldOut: false
+    soldOut: false,
+    images:[
+      {full:"images/dodecadron-01-full.gif",thumb:"images/dodecadron-01-thumb.gif"},
+      {full:"images/dodecadron-02-full.gif",thumb:"images/dodecadron-02-thumb.gif"},
+      {full:"images/dodecadron-03-full.gif",thumb:"images/dodecadron-03-thumb.gif"}
+    ]
   },
   {
     name : "Pentagonal Gem",
     price: 5.95,
     description: "Yet, another beaultiful gem. Yet, another beaultiful gem. Yet, another beaultiful gem.",
     canPurchase: true,
-    soldOut: false
+    soldOut: false,
+    images:[
+      {full:"images/pentagonal-01-full.gif",thumb:"images/pentagonal-01-thumb.gif"},
+      {full:"images/pentagonal-02-full.gif",thumb:"images/pentagonal-02-thumb.gif"},
+      {full:"images/pentagonal-03-full.gif",thumb:"images/pentagonal-03-thumb.gif"}
+    ]
   },
   {
     name : "Pentagonalx Gem",
     price: 5.95,
     description: "Yet, another beaultiful gem. Yet, another beaultiful gem. Yet, another beaultiful gem.",
     canPurchase: true,
-    soldOut: false
+    soldOut: false,
+    images:[
+      {full:"images/pentagonalx-01-full.gif",thumb:"images/pentagonalx-01-thumb.gif"},
+      {full:"images/pentagonalx-02-full.gif",thumb:"images/pentagonalx-02-thumb.gif"},
+      {full:"images/pentagonalx-03-full.gif",thumb:"images/pentagonalx-03-thumb.gif"}
+    ]
   }];
 })();
