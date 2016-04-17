9a26c65612e55febdd5c6028d8ec07bb9979cfe2
Ademir C. Gabardo <ademir.gabbardo@gmail.com>
2016-04-17 19:25:43 +1000

    Level 4 completed, adding directives to App.js file

--- js/app.js
+++ js/app.js
@@ -27,11 +27,32 @@
 
   app.controller('ReviewController', function() {
     this.review = {};
-
     this.addReview = function(product) {
       this.review.createdOn = Date.now();
       product.reviews.push(this.review);
       this.review = {};
+    };
+  });
+
+  app.directive('productTitle', function(){
+    return{
+      restrict: 'E',
+      templateUrl: 'product_title.html'
+    };
+  });
+
+  app.directive('productPanels', function(){
+    return{
+      restrict: 'E',
+      templateUrl: 'product-panels.html',
+      controller:function(){
+        this.review = {};
+        this.addReview = function(product) {
+        this.review.createdOn = Date.now();
+        product.reviews.push(this.review);
+        this.review = {};
+        }
+      }
     };
   });
 
